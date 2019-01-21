e7676854a456912c235c14bdae00fe1c0ee53d28
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-01-21 12:24:11 +0800

    将字典表放到基础库中，服务端每个表必须指令所在库

--- ProjectBussInoma/udf/com/kaifantech/init/sys/AppSystemInitDB.java
+++ ProjectBussInoma/udf/com/kaifantech/init/sys/AppSystemInitDB.java
@@ -1,6 +1,6 @@
 package com.kaifantech.init.sys;
 
-public class SystemInitDB {
+public class AppSystemInitDB extends BaseSystemInitDB {
 	private static final String SYSTEM_INIT_DB_KEY = "kf_csy_daj_";
 
 	public static final String OP_DB_NAME = SYSTEM_INIT_DB_KEY + "agv";
@@ -8,8 +8,6 @@ public class SystemInitDB {
 	public static final String IOT_DB_NAME = SYSTEM_INIT_DB_KEY + "iot";
 	public static final String DE_DB_NAME = SYSTEM_INIT_DB_KEY + "de";
 	public static final String WMS_DB_NAME = SYSTEM_INIT_DB_KEY + "wms";
-
-	public static final String KF_BAK = "kf_bak";
 
 	public static final String CONF_DB_NAME = SYSTEM_INIT_DB_KEY + "_conf";
 	public static final String CACHE_CONF_DB_NAME = SYSTEM_INIT_DB_KEY + "_cache_conf";
