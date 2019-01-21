f4a68018fda326f7f79a0aaded5b16cb436baf1a
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-01-21 14:21:36 +0800

    简化数据库名称

--- ProjectBussInoma/udf/com/kaifantech/init/sys/AppSystemInitDB.java
+++ ProjectBussInoma/udf/com/kaifantech/init/sys/AppSystemInitDB.java
@@ -3,13 +3,13 @@ package com.kaifantech.init.sys;
 public class AppSystemInitDB extends BaseSystemInitDB {
 	private static final String SYSTEM_INIT_DB_KEY = "kf_csy_daj_";
 
-	public static final String OP_DB_NAME = SYSTEM_INIT_DB_KEY + "agv";
-	public static final String BUSS_DB_NAME = SYSTEM_INIT_DB_KEY + "acs";
-	public static final String IOT_DB_NAME = SYSTEM_INIT_DB_KEY + "iot";
-	public static final String DE_DB_NAME = SYSTEM_INIT_DB_KEY + "de";
-	public static final String WMS_DB_NAME = SYSTEM_INIT_DB_KEY + "wms";
+	public static final String OP_DB = SYSTEM_INIT_DB_KEY + "agv";
+	public static final String BUSS_DB = SYSTEM_INIT_DB_KEY + "acs";
+	public static final String IOT_DB = SYSTEM_INIT_DB_KEY + "iot";
+	public static final String DE_DB = SYSTEM_INIT_DB_KEY + "de";
+	public static final String WMS_DB = SYSTEM_INIT_DB_KEY + "wms";
 
-	public static final String CONF_DB_NAME = SYSTEM_INIT_DB_KEY + "_conf";
-	public static final String CACHE_CONF_DB_NAME = SYSTEM_INIT_DB_KEY + "_cache_conf";
-	public static final String CACHE_SOCKET_DB_NAME = SYSTEM_INIT_DB_KEY + "_cache_socket";
+	public static final String CONF_DB = SYSTEM_INIT_DB_KEY + "_conf";
+	public static final String CACHE_CONF_DB = SYSTEM_INIT_DB_KEY + "_cache_conf";
+	public static final String CACHE_SOCKET_DB = SYSTEM_INIT_DB_KEY + "_cache_socket";
 }
