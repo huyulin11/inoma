0fca987d510c7237c8dac63fd65b75d770754d93
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-03-11 16:47:24 +0800

    ss

--- ProjectBussInoma/udf/com/kaifantech/init/sys/AppBusinessInfo.java
+++ ProjectBussInoma/udf/com/kaifantech/init/sys/AppBusinessInfo.java
@@ -2,5 +2,5 @@ package com.kaifantech.init.sys;
 
 public class AppBusinessInfo extends BaseBusinessInfo {
 	public static final Clients CURRENT_CLIENT = Clients.INOMA;
-	public static final Projects CURRENT_PROJECT = Projects.XUZHOU_WEIWEI;
+	public static final Projects CURRENT_PROJECT = Projects.INOMA_WEIWEI;
 }
