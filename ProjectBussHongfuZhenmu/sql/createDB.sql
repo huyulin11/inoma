9590f31b1eacb8c35f4fbfbfe1dc2c037e2e8e28
huyulin11@sina.cn <huyulin11@sina.cn>
2019-06-01 12:39:47 +0800

    建库脚本

---
+++ ProjectBussHongfuZhenmu/sql/createDB.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE `kf_inoma_weiwei__conf`;
+CREATE DATABASE `kf_inoma_weiwei_acs`;
+CREATE DATABASE `kf_inoma_weiwei_agv`;
+CREATE DATABASE `kf_inoma_weiwei_iot`;
+CREATE DATABASE `kf_inoma_weiwei_wms`;
