d7c13db6b998ce4c6ea0e9acc0591b457cb5b3b1
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-06-01 15:25:26 +0800

    增加将静态货位信息写到json中逻辑

--- ProjectBussHongfuZhenmu/udf/com/kaifantech/init/sys/AppBusinessInfo.java
+++ ProjectBussHongfuZhenmu/udf/com/kaifantech/init/sys/AppBusinessInfo.java
@@ -1,6 +1,20 @@
 package com.kaifantech.init.sys;
 
+import java.io.IOException;
+
+import com.alibaba.fastjson.JSONArray;
+import com.kaifantech.component.cache.worker.AppContextStaticBeanFactory;
+import com.kaifantech.component.dao.alloc.AllocItemDao;
+
 public class AppBusinessInfo extends BaseBusinessInfo {
 	public static final Clients CURRENT_CLIENT = Clients.INOMA;
 	public static final Projects CURRENT_PROJECT = Projects.HONGFU_ZHENMU;
+
+	public static void createStaticFile() throws IOException {
+		BaseBusinessInfo.createStaticFile();
+		AllocItemDao allocItemDao = AppContextStaticBeanFactory.getBean(AllocItemDao.class);
+		String info;
+		info = JSONArray.toJSON(allocItemDao.getAllocs()).toString();
+		AppBusinessInfo.createFile(AppBusinessInfo.getProjJsonsPath() + "allocs/", "allocs" + ".json", info);
+	}
 }
