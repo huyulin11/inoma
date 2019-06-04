1e6ff3c50daecef47ab49cd3f2e110305fcdaf15
huyulin@calculatedfun.com <huyulin@calculatedfun.com>
2019-06-04 10:50:30 +0800

    s

--- ProjectBussHongfuZhenmu/sql/按照货位生成任务数据.sql
+++ ProjectBussHongfuZhenmu/sql/按照货位生成任务数据.sql
@@ -1,40 +1,216 @@
-delimiter //                            #定义标识符为双斜杠
-drop procedure if exists test;          #如果存在test存储过程则删除
-create procedure test()                 #创建无参存储过程,名称为test
-begin
-    declare i int;                      #申明变量
-    set i = 1;                          #变量赋值
-DELETE from `kf_inoma_weiwei_agv`.`singletask_info` WHERE id>0;
-    while i <= 8 do                     #结束循环的条件: 当i大于10时跳出while循环
-    if i=1 then
-SET @agvid = 1, @tasktypeCn = '存货', @tasktypeEn = 'RECEIPT', @tasktypeSt = 'Q', @lapId = '1';
-ELSEIF i=2 then
-SET @agvid = 1, @tasktypeCn = '存货', @tasktypeEn = 'RECEIPT', @tasktypeSt = 'Q', @lapId = '2';
-ELSEIF i=3 then
-SET @agvid = 1, @tasktypeCn = '取货', @tasktypeEn = 'SHIPMENT', @tasktypeSt = 'F', @lapId = '1';
-ELSEIF i=4 then
-SET @agvid = 1, @tasktypeCn = '取货', @tasktypeEn = 'SHIPMENT', @tasktypeSt = 'F', @lapId = '2';               
-ELSEIF i=5 then
-SET @agvid = 2, @tasktypeCn = '存货', @tasktypeEn = 'RECEIPT', @tasktypeSt = 'Q', @lapId = '1';
-ELSEIF i=6 then
-SET @agvid = 2, @tasktypeCn = '存货', @tasktypeEn = 'RECEIPT', @tasktypeSt = 'Q', @lapId = '2';
-ELSEIF i=7 then
-SET @agvid = 2, @tasktypeCn = '取货', @tasktypeEn = 'SHIPMENT', @tasktypeSt = 'F', @lapId = '1';
-ELSEIF i=8 then
-SET @agvid = 2, @tasktypeCn = '取货', @tasktypeEn = 'SHIPMENT', @tasktypeSt = 'F', @lapId = '2';
-END if;
+--WARNING! ERRORS ENCOUNTERED DURING SQL PARSING!
+--WARNING! ERRORS ENCOUNTERED DURING SQL PARSING!
+--WARNING! ERRORS ENCOUNTERED DURING SQL PARSING!
+--WARNING! ERRORS ENCOUNTERED DURING SQL PARSING!
+delimiter //                            
 
-INSERT INTO `kf_inoma_weiwei_agv`.`singletask_info` 
-		(`environment`, `taskName`, `taskText`, `taskType`, `allocid`,
- 		`environmentId`, `agvId`, `lapId`, `issend`, `delflag`)
-SELECT 
-	'1',CONCAT(@agvid,'-',@tasktypeSt,@lapId,'-',`text`, '.xml')
- 	, CONCAT('@','车',@agvid,'@',@tasktypeCn,'点',@lapId,'@',`text`), @tasktypeEn,
-	`id`, '1', @agvid, @lapId, '1', '0'
-FROM kf_inoma_weiwei_wms.allocation_item_info
-ORDER BY id;
-        set i = i + 1;                  #循环一次,i加一
-    end while;                          #结束while循环
-end
-//                                      #结束定义语句
-call test();                            #调用存储过程
+DROP PROCEDURE
+
+IF EXISTS test;
+	CREATE PROCEDURE test ()
+
+BEGIN
+	DECLARE i INT;
+
+	SET i = 1;
+
+	TRUNCATE `kf_inoma_weiwei_agv`.`singletask_info`;
+
+	INSERT INTO `singletask_info` (
+		`environment`
+		,`id`
+		,`taskname`
+		,`tasktext`
+		,`tasktype`
+		,`allocoptype`
+		,`allocid`
+		,`environmentid`
+		,`agvid`
+		,`lapid`
+		,`issend`
+		,`delflag`
+		,`orderby`
+		,`createtime`
+		)
+	VALUES (
+		1
+		,- 4
+		,'C1.xml'
+		,'1号车前往充电'
+		,'GOTO_CHARGE'
+		,'1'
+		,0
+		,1
+		,1
+		,1
+		,1
+		,0
+		,0
+		,'2019-06-03 21:50:51'
+		)
+		,(
+		1
+		,- 3
+		,'D1.xml'
+		,'1号车充电返回'
+		,'BACK_CHARGE'
+		,'1'
+		,0
+		,1
+		,1
+		,1
+		,1
+		,0
+		,0
+		,'2019-06-03 21:51:16'
+		)
+		,(
+		1
+		,- 2
+		,'C2.xml'
+		,'2号车前往充电'
+		,'GOTO_CHARGE'
+		,'1'
+		,0
+		,1
+		,1
+		,1
+		,1
+		,0
+		,0
+		,'2019-06-03 21:51:57'
+		)
+		,(
+		1
+		,- 1
+		,'D2.xml'
+		,'2号车充电返回'
+		,'BACK_CHARGE'
+		,'1'
+		,0
+		,1
+		,1
+		,1
+		,1
+		,0
+		,0
+		,'2019-06-03 21:52:25'
+		);
+
+	SET @che = 'AGV'
+		,@dian = '位置';
+
+	WHILE i <= 8 do
+		IF i = 1 then
+			SET @agvid = 1
+				,@tasktypeCn = '存货'
+				,@tasktypeEn = 'RECEIPT'
+				,@tasktypeSt = 'Q'
+				,@lapId = '1';
+
+	ELSEIF i = 2 then
+
+	SET @agvid = 1
+		,@tasktypeCn = '存货'
+		,@tasktypeEn = 'RECEIPT'
+		,@tasktypeSt = 'Q'
+		,@lapId = '2';
+
+	ELSEIF i = 3 then
+
+	SET @agvid = 1
+		,@tasktypeCn = '取货'
+		,@tasktypeEn = 'SHIPMENT'
+		,@tasktypeSt = 'F'
+		,@lapId = '1';
+
+	ELSEIF i = 4 then
+
+	SET @agvid = 1
+		,@tasktypeCn = '取货'
+		,@tasktypeEn = 'SHIPMENT'
+		,@tasktypeSt = 'F'
+		,@lapId = '2';
+
+	ELSEIF i = 5 then
+
+	SET @agvid = 2
+		,@tasktypeCn = '存货'
+		,@tasktypeEn = 'RECEIPT'
+		,@tasktypeSt = 'Q'
+		,@lapId = '1';
+
+	ELSEIF i = 6 then
+
+	SET @agvid = 2
+		,@tasktypeCn = '存货'
+		,@tasktypeEn = 'RECEIPT'
+		,@tasktypeSt = 'Q'
+		,@lapId = '2';
+
+	ELSEIF i = 7 then
+
+	SET @agvid = 2
+		,@tasktypeCn = '取货'
+		,@tasktypeEn = 'SHIPMENT'
+		,@tasktypeSt = 'F'
+		,@lapId = '1';
+
+	ELSEIF i = 8 then
+
+	SET @agvid = 2
+		,@tasktypeCn = '取货'
+		,@tasktypeEn = 'SHIPMENT'
+		,@tasktypeSt = 'F'
+		,@lapId = '2';
+END
+
+IF ;
+	INSERT INTO `kf_inoma_weiwei_agv`.`singletask_info` (
+		`environment`
+		,`taskName`
+		,`taskText`
+		,`taskType`
+		,`allocid`
+		,`environmentId`
+		,`agvId`
+		,`lapId`
+		,`issend`
+		,`delflag`
+		)
+	SELECT '1'
+		,CONCAT (
+			@agvid
+			,'-'
+			,@tasktypeSt
+			,@lapId
+			,'-'
+			,`text`
+			,'.xml'
+			)
+		,CONCAT (
+			'@'
+			,@che
+			,@agvid
+			,'@'
+			,@tasktypeCn
+			,@dian
+			,@lapId
+			,'@'
+			,`text`
+			)
+		,@tasktypeEn
+		,`id`
+		,'1'
+		,@agvid
+		,@lapId
+		,'1'
+		,'0'
+	FROM kf_inoma_weiwei_wms.allocation_item_info
+	ORDER BY id;
+
+SET i = i + 1;END
+
+WHILE ;END
+	//                                      
+	call test();
